84b8f5c030feb33c4b9b28534b244325043e038a
Joeri Bakker <bakkerjoeri@gmail.com>
2019-08-10 13:41:33 +0200

    Remove dependency on logo

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,33 +12,8 @@
 	</div>
 </template>
 
-<script>
-	import Logo from "./../components/Logo.vue";
-	
-	export default {
-		components: {
-			Logo,
-		},
-	}
-</script>
-
 <style lang="scss">
 	* {
 		box-sizing: border-box;
-	}
-	
-	body {
-		margin: 0;
-	}
-	
-	.header {
-		display: flex;
-		background-color: #df5b57;
-		justify-content: center;
-		padding: 10px 20px;
-	}
-	
-	.header__logo {
-		width: 180px;
 	}
 </style>
